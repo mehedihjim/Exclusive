577482eabc9a3212cc78ab54ec2848fb83e2e538
mehedihjim <mhjim.info@gmail.com>
2024-10-13 13:43:00 +0600

    Slider Customization done

--- src/components/BannerSlider.jsx
+++ src/components/BannerSlider.jsx
@@ -2,6 +2,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import React from "react";
 import Slider from "react-slick";
+import './BannerSlider.css'
 import { bannerData } from '../constant/assets'
 import { Link } from "react-router-dom";
 
@@ -10,9 +11,34 @@ const BannerSlider = () => {
     const settings = {
         dots: true,
         infinite: true,
-        speed: 300,
+        speed: 500,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        speed: 1000,
+        autoplaySpeed: 2000,
+        appendDots: dots => (
+            <div
+                style={{
+                    margin: "21px",
+                    padding: "10px"
+                }}
+            >
+                <ul style={{ margin: "0px" }}> {dots} </ul>
+            </div>
+        ),
+        customPaging: i => (
+            <div
+                style={{
+                    width: "12px",
+                    height: "12px",
+                    backgroundColor: "#ffffff50",
+                    borderRadius: "100%",
+                }}
+            >
+            </div>
+        ),
     };
 
     return (
